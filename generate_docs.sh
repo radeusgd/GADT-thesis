0add4ebe656560108a2b16aa66cd1c80fb41fe64
Radosław Waśko <wasko.radek@gmail.com>
2021-12-03 01:57:46 +0100

    Rename docs

--- generate_docs.sh
+++ generate_docs.sh
@@ -7,28 +7,28 @@ DOCOPTS="-utf8 --toc --toc-depth 0 --html --interpolate \
   --index indexpage --no-lib-name --parse-comments \
   --with-header $EXTRA_DIR/header.html --with-footer $EXTRA_DIR/footer.html"
 
-rm -rf doc
+rm -rf docs
 
-mkdir -p doc/lambda2Gmu
+mkdir -p docs/lambda2Gmu
 pushd lambda2Gmu
-coqdoc $DOCOPTS -R . GMu -d ../doc/lambda2Gmu *.v
-cp $EXTRA_DIR/resources/* ../doc/lambda2Gmu/
+coqdoc $DOCOPTS -R . GMu -d ../docs/lambda2Gmu *.v
+cp $EXTRA_DIR/resources/* ../docs/lambda2Gmu/
 popd
 
-mkdir -p doc/lambda2Gmu_annotated
+mkdir -p docs/lambda2Gmu_annotated
 pushd lambda2Gmu_annotated
-coqdoc $DOCOPTS -R . GMuAnnot --external ../lambda2Gmu GMu -d ../doc/lambda2Gmu_annotated *.v
-cp $EXTRA_DIR/resources/* ../doc/lambda2Gmu_annotated/
+coqdoc $DOCOPTS -R . GMuAnnot --external ../lambda2Gmu GMu -d ../docs/lambda2Gmu_annotated *.v
+cp $EXTRA_DIR/resources/* ../docs/lambda2Gmu_annotated/
 popd
 
-mkdir -p doc/translation_pdot
+mkdir -p docs/translation_pdot
 pushd translation_pdot
-coqdoc $DOCOPTS -R . Top --external ../lambda2Gmu GMu --external ../lambda2Gmu_annotated GMuAnnot -d ../doc/translation_pdot *.v
-cp $EXTRA_DIR/resources/* ../doc/translation_pdot/
+coqdoc $DOCOPTS -R . Top --external ../lambda2Gmu GMu --external ../lambda2Gmu_annotated GMuAnnot -d ../docs/translation_pdot *.v
+cp $EXTRA_DIR/resources/* ../docs/translation_pdot/
 popd
 
-mkdir -p doc/translation_extended
+mkdir -p docs/translation_extended
 pushd translation_extended
-coqdoc $DOCOPTS -R . Top --external ../lambda2Gmu GMu --external ../lambda2Gmu_annotated GMuAnnot -d ../doc/translation_extended *.v
-cp $EXTRA_DIR/resources/* ../doc/translation_extended/
+coqdoc $DOCOPTS -R . Top --external ../lambda2Gmu GMu --external ../lambda2Gmu_annotated GMuAnnot -d ../docs/translation_extended *.v
+cp $EXTRA_DIR/resources/* ../docs/translation_extended/
 popd
